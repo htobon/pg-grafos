575ff8d2570ceb8eb0b790eef5767502dd0849d5
verito4891@gmail.com <verito4891@gmail.com@db5dec38-88e3-11de-b5b6-bfb8bb3c0290>
2009-12-11 15:06:51 +0000

    Obtener atributos de nodos y aristas

--- PG/ctrl/Ctrl.java
+++ PG/ctrl/Ctrl.java
@@ -21,11 +21,11 @@ public class Ctrl {
 		return ServiciosBD.crearConexion(servidor, puerto, usuario, clave,
 				nombreBD);
 	}
-	
+
 	public static int[] getCodigosNodos() {
 		int[] codigos = new int[grafo.getNodos().size()];
-		int cont=0;
-		for(Iterator<Nodo> i = grafo.getNodos().iterator(); i.hasNext(); cont++) {
+		int cont = 0;
+		for (Iterator<Nodo> i = grafo.getNodos().iterator(); i.hasNext(); cont++) {
 			codigos[cont] = i.next().getCodigo();
 		}
 		return codigos;
@@ -54,14 +54,16 @@ public class Ctrl {
 			aristas.add(new Arista(codigo, tipo, grafo.getNodo(nodos[0]), grafo
 					.getNodo(nodos[1])));
 		}
-		
+
 		// Asignando aristas al grafo.
 		grafo.setAristas(aristas);
-		
+
 		// Asignando atributos a las aristas.
-		for(int codigo : codigosAristas) {
-			// Pido las propiedades de cada arista se los asigno a sus atributos.
-			grafo.getArista(codigo).setAtributos(ServiciosBD.getAtributosArista(codigo));
+		for (int codigo : codigosAristas) {
+			// Pido las propiedades de cada arista se los asigno a sus
+			// atributos.
+			grafo.getArista(codigo).setAtributos(
+					ServiciosBD.getAtributosArista(codigo));
 		}
 	}
 
@@ -82,8 +84,8 @@ public class Ctrl {
 		// Asignando atributos a todos los nodos.
 		for (int codigo : codigosNodos) {
 			// Pido propiedades de este nodo y se los asigno a sus atributos.
-			grafo.getNodo(codigo)
-					.setAtributos(ServiciosBD.getAtributosNodo(codigo));
+			grafo.getNodo(codigo).setAtributos(
+					ServiciosBD.getAtributosNodo(codigo));
 		}
 	}
 
@@ -103,60 +105,82 @@ public class Ctrl {
 	public static HashSet<Arista> getAristas() {
 		return grafo.getAristas();
 	}
-	
-	public static void llenarMapaColores(){
-		HashMap<String,Color> coloresNodos= new HashMap<String, Color>();	
-		HashMap<String,Color> coloresAristas= new HashMap<String, Color>();
-		HashSet<Nodo> nodos= grafo.getNodos();
-		HashSet<Arista> aristas= grafo.getAristas();
-		Random r =  new Random();
-		
+
+	public static void llenarMapaColores() {
+		HashMap<String, Color> coloresNodos = new HashMap<String, Color>();
+		HashMap<String, Color> coloresAristas = new HashMap<String, Color>();
+		HashSet<Nodo> nodos = grafo.getNodos();
+		HashSet<Arista> aristas = grafo.getAristas();
+		Random r = new Random();
+
 		for (Nodo nodo : nodos) {
-			if(!(coloresNodos.containsKey(nodo.getTipo()))){
-				coloresNodos.put(nodo.getTipo(), new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256)));
+			if (!(coloresNodos.containsKey(nodo.getTipo()))) {
+				coloresNodos.put(nodo.getTipo(), new Color(r.nextInt(256), r
+						.nextInt(256), r.nextInt(256)));
 			}
 		}
 		grafo.setColoresNodos(coloresNodos);
-		
+
 		for (Arista arista : aristas) {
-			if(!(coloresAristas.containsKey(arista.getTipo()))){
-				coloresAristas.put(arista.getTipo(), new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256)));
+			if (!(coloresAristas.containsKey(arista.getTipo()))) {
+				coloresAristas.put(arista.getTipo(), new Color(r.nextInt(256),
+						r.nextInt(256), r.nextInt(256)));
 			}
 		}
 		grafo.setColoresAristas(coloresAristas);
 	}
-	
-	public void asignarColoresNodos(){
-		HashSet<Nodo> nodos= grafo.getNodos();
-		HashMap<String,Color> coloresNodos= new HashMap<String, Color>();	
+
+	public static void asignarColoresNodos() {
+		HashSet<Nodo> nodos = grafo.getNodos();
+		HashMap<String, Color> coloresNodos = new HashMap<String, Color>();
 		for (Nodo nodo : nodos) {
 			nodo.setColor(coloresNodos.get(nodo.getTipo()));
 		}
 	}
-	
-	public void asignarColoresAristas(){
-		HashSet<Arista> aristas= grafo.getAristas();
-		HashMap<String,Color> coloresAristas= new HashMap<String, Color>();	
+
+	public static void asignarColoresAristas() {
+		HashSet<Arista> aristas = grafo.getAristas();
+		HashMap<String, Color> coloresAristas = new HashMap<String, Color>();
 		for (Arista arista : aristas) {
 			arista.setColor(coloresAristas.get(arista.getTipo()));
 		}
 	}
-	
-	public Color getColorNodo(int codNodo){
-		HashSet<Nodo> nodos= grafo.getNodos();
+
+	public static Color getColorNodo(int codNodo) {
+		HashSet<Nodo> nodos = grafo.getNodos();
 		for (Nodo nodo : nodos) {
-			if(nodo.getCodigo()== codNodo){
+			if (nodo.getCodigo() == codNodo) {
 				return nodo.getColor();
 			}
 		}
 		return null;
 	}
-	
-	public Color getColorArista(int codArista){
-		HashSet<Arista> aristas= grafo.getAristas();
+
+	public static Color getColorArista(int codArista) {
+		HashSet<Arista> aristas = grafo.getAristas();
 		for (Arista arista : aristas) {
-			if(arista.getCodigo()== codArista){
+			if (arista.getCodigo() == codArista) {
 				return arista.getColor();
+			}
+		}
+		return null;
+	}
+
+	public static HashMap<String, Object> getAtributosNodo(int codNodo) {
+		HashSet<Nodo> nodos = grafo.getNodos();
+		for (Nodo nodo : nodos) {
+			if (nodo.getCodigo() == codNodo) {
+				return nodo.getAtributos();
+			}
+		}
+		return null;
+	}
+
+	public static HashMap<String, Object> getAtributosArista(int codArista) {
+		HashSet<Arista> aristas = grafo.getAristas();
+		for (Arista arista : aristas) {
+			if (arista.getCodigo() == codArista) {
+				return arista.getAtributos();
 			}
 		}
 		return null;
