d7c3021c3b44e2295c902f1af0b9acfe2ef97bcc
HTOBON <HTOBON@db5dec38-88e3-11de-b5b6-bfb8bb3c0290>
2009-12-28 20:01:36 +0000

    implementando picking 2

--- trunk/PG/vista3d/MousePicking.java
+++ trunk/PG/vista3d/MousePicking.java
@@ -1,5 +1,7 @@
 package vista3d;
 
+import vista.Principal;
+
 import com.jme.input.MouseInput;
 import com.jme.input.action.InputActionEvent;
 import com.jme.input.action.MouseInputAction;
@@ -23,6 +25,7 @@ public class MousePicking extends MouseInputAction {
     private int hits = 0;
     private int shots = 0;
     private String hitItems;
+    private String texto;
 
     public MousePicking(Camera camera, Node scene) {
         this.camera = camera;
@@ -45,6 +48,8 @@ public class MousePicking extends MouseInputAction {
             hits += results.getNumber();
             hitItems = "";
             if(results.getNumber() > 0) {
+            	texto = results.getPickData(0).getTargetMesh().getName();
+            	Principal.jtaInfo.setText(texto);
                 for(int i = 0; i < results.getNumber(); i++) {
                     hitItems += results.getPickData(i).getTargetMesh().getName() + " " + results.getPickData(i).getDistance();
                     if(i != results.getNumber() -1) {
