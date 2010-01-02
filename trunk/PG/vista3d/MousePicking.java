d1c8a4aa8800458f5993aa95d6444128b3ee69b0
HTOBON <HTOBON@db5dec38-88e3-11de-b5b6-bfb8bb3c0290>
2010-01-02 14:06:18 +0000

    reformando random colors

--- trunk/PG/vista3d/MousePicking.java
+++ trunk/PG/vista3d/MousePicking.java
@@ -10,56 +10,65 @@ import com.jme.intersection.PickResults;
 import com.jme.math.Ray;
 import com.jme.renderer.Camera;
 import com.jme.scene.Node;
-import com.jme.scene.Text;
 
 /**
  * <code>MousePick</code>
+ * 
  * @author Mark Powell
  * @version
  */
 public class MousePicking extends MouseInputAction {
 
-    private Camera camera;
-    private Node scene;
-    private float shotTime = 0;
-    private int hits = 0;
-    private int shots = 0;
-    private String hitItems;
-    private String texto;
+	private Camera camera;
+	private Node scene;
+	private float shotTime = 0;
+	private int hits = 0;
+	private int shots = 0;
+	private String hitItems;
+	private String texto;
+	private final Ray ray = new Ray();
 
-    public MousePicking(Camera camera, Node scene) {
-        this.camera = camera;
-        this.scene = scene;
-    }
-    /* (non-Javadoc)
-     * @see com.jme.input.action.MouseInputAction#performAction(float)
-     */
-    public void performAction(InputActionEvent evt) {
-        shotTime += evt.getTime();
-        
-        if( MouseInput.get().isButtonDown(1) && shotTime > 0.1f) {
-            shotTime = 0;
-            Ray ray = new Ray(camera.getLocation(), camera.getDirection()); // camera direction is already normalized
-            PickResults results = new BoundingPickResults();
-            results.setCheckDistance(true);
-            scene.findPick(ray,results);
+	public MousePicking(Camera camera, Node scene) {
+		this.camera = camera;
+		this.scene = scene;
+	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.jme.input.action.MouseInputAction#performAction(float)
+	 */
+	public void performAction(InputActionEvent evt) {
+		shotTime += evt.getTime();
 
-            hits += results.getNumber();
-            hitItems = "";
-            if(results.getNumber() > 0) {
-            	texto = results.getPickData(0).getTargetMesh().getName();
-            	Principal.jtaInfo.setText(texto);
-                for(int i = 0; i < results.getNumber(); i++) {
-                    hitItems += results.getPickData(i).getTargetMesh().getName() + " " + results.getPickData(i).getDistance();
-                    if(i != results.getNumber() -1) {
-                        hitItems += ", ";
-                    }
-                }
-            }
-            shots++;
-            results.clear();
-            System.out.println(hitItems);
-        }
-    }
+		if (MouseInput.get().isButtonDown(1) && shotTime > 0.1f) {
+			shotTime = 0;
+			ray.setOrigin(camera.getLocation());
+			ray.setDirection(camera.getDirection());
+			// camera direction is already normalized
+			ray.getDirection().normalizeLocal();
+			
+			PickResults results = new BoundingPickResults();
+			results.setCheckDistance(true);
+			scene.findPick(ray, results);
+
+			hits += results.getNumber();
+			hitItems = "";
+			if (results.getNumber() > 0) {
+				texto = results.getPickData(0).getTargetMesh().getName();
+				Principal.jtaInfo.setText(texto);
+				for (int i = 0; i < results.getNumber(); i++) {
+					hitItems += results.getPickData(i).getTargetMesh()
+							.getName()
+							+ " " + results.getPickData(i).getDistance();
+					if (i != results.getNumber() - 1) {
+						hitItems += ", ";
+					}
+				}
+			}
+			shots++;
+			results.clear();
+			System.out.println(hitItems);
+		}
+	}
 }
